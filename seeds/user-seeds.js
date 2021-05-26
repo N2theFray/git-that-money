dce9ca3bf2b670c859450df251c85106fd419e50
Scuba Steve <78673754+blacksteven1@users.noreply.github.com>
2021-05-27 02:11:15 +0300

    seeded files

---
+++ seeds/user-seeds.js
@@ -0,0 +1,19 @@
+const { User } = require('../models');
+
+const userdata = [
+  {
+    username: 'bobthebuilder',
+    email: 'email@email.com',
+    password: 'password123'
+  },
+  {
+    username: 'sirmixalot',
+    email: 'omgbecky@email.com',
+    password: 'password123'
+  }
+  
+];
+
+const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
+
+module.exports = seedUsers;
